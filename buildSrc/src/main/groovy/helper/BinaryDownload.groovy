afdffef7d675265ffcc2ec1c74cd8b9446f06508
Brigitte Hulliger <brigitte.hulliger@gmail.com>
2014-08-07 13:51:25 +0200

    buildpipeline with deployment and functional tests

---
+++ buildSrc/src/main/groovy/helper/BinaryDownload.groovy
@@ -0,0 +1,20 @@
+package helper
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+class BinaryDownload extends DefaultTask {
+
+	@Input
+	String sourceUrl
+
+	@OutputFile
+	File targetBinary
+
+	@TaskAction
+	void download() {
+		ant.get(src: sourceUrl, dest: targetBinary)
+	}
+}
